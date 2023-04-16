b62b40214d1c0173d24a362acec164383c084ca9
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-16 12:45:06 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,14 @@
+// const name = {
+//   mayur: true,
+//   shubham: false,
+//   ritesh: 67,
+//   rohan: undefined,
+// };
+// console.log(name["rohan"]);
+//Write a javascript program to store name phone number and marks of student using object.
+const student = {
+  name: "mayur",
+  phonenumber: "8379921692",
+  marks: "88%",
+};
+console.log(student["name"]);

3a94f9efbaedddd3c9ab4693134965c0dd9389ef
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-16 12:06:44 +0530

    Add files via upload

---
+++ premitive.js
@@ -0,0 +1,10 @@
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("511");
+let e = "mayur";
+let f = Symbol("Mayur is claver ");
+console.log(a, b, c, d, e, f);
+console.log(typeof b);
+
+//object

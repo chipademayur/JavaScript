0e588ad035b3a66d7fd6fc2a37b0af7478c938fd
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-16 13:12:11 +0530

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,25 @@
+//create a variable of type string and try to add a number to it?
+let a = "satish";
+let b = "Mayur";
+let c = "10";
+console.log(b + a + c);
+//Use a type of operator find the type of operator of last question
+
+console.log(typeof a);
+//write a javascript try to add a new key to const object
+try to add key to const
+const a1 = {
+  Name: "Mayur",
+  school: "rmv",
+};
+a1["friend"] = "ritesh";
+console.log(a1);
+
+//write a javascript program to create a word meanning dectionary
+
+const dect = {
+  Smart: "cleaver",
+  nearby: "close",
+  whole: "total",
+};
+console.log(dect.whole);

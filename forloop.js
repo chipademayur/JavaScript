3e1e9f4af941d089233d072e1ecf795f365f27bf
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-20 16:22:15 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,10 @@
+//program to add first n natural number
+
+let sum = 0;
+let n = 4;
+// let n = prompt("enter the value of n");
+// n = Number.parseInt(n);
+for (i = 0; i < n; i++) {
+  sum = sum + (i + 1);
+}
+console.log(sum);

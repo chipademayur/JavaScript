d08f5d39b084c0b90632c6bd5e91f5ed70906bba
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-20 23:22:14 +0530

    Add files via upload

---
+++ whileloop.js
@@ -0,0 +1,14 @@
+// while loop
+let number = 10;
+let i = 0;
+while (i < number) {
+  console.log(i);
+  i++;
+}
+
+// do while loop
+let number1 = 10;
+do {
+  console.log(i);
+  i++;
+} while (i < number1);

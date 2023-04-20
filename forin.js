4a847b5fc7b251b1756103ca51b8d088cb95bbb2
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-20 16:27:36 +0530

    Add files via upload

---
+++ forin.js
@@ -0,0 +1,10 @@
+// for in loop is use for iterate overall enamerable of an object or to walk overall keys of an object for in loop is use
+let student = {
+  Mayuur: 90,
+  ritesh: 95,
+  vishu: 100,
+  prajwel: 90,
+};
+for (let a in student) {
+  console.log("Marks of " + a + " are " + student[a]);
+}

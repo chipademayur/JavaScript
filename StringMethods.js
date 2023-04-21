05e2a7f01c5168d678102f87aa0225236fbc63b4
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-21 14:57:38 +0530

    Add files via upload

---
+++ StringMethods.js
@@ -0,0 +1,14 @@
+// String Methods in javaScript
+
+let boy1 = "Mayur";
+console.log(boy1.toUpperCase()); //For Converting string in uppercase format
+
+console.log(boy1.toLowerCase()); //for converting string in lower case.
+
+console.log(boy1.length); //for finding the length of string
+
+console.log(boy1.slice(2, 4)); //for slice in two portation
+let name1 = "ram";
+console.log(name1.replace("ram", "ramdev")); //for repacing the previous string to new one.
+
+console.log(boy1.concat(" is best friend of", name1)); //for concatination of two strings

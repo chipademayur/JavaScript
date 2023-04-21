2933252030d2f943bd5d04f94b82cefed400dfd8
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-21 09:32:54 +0530

    Add files via upload

---
+++ function1.js
@@ -0,0 +1,12 @@
+// simple function code
+function addavg(x, y) {
+  console.log("Done");
+  return Math.round(1 + (x + y) / 2);
+}
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log("addition Average of a and b ", addavg(a, b));
+console.log("addition average of b and c ", addavg(b, c));
+console.log("addition average of a and c ", addavg(a, c));

8aeadccef9a4bc8eac7a630d79ac0b6ca65017b0
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-23 22:27:42 +0530

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,7 @@
+let arr = [20, 3, , 33, 44, 55, 1, 2];
+let a = arr.filter((a) => {
+  return a < 10;
+});
+console.log(a);
+
+// filter method in js is use for filter out the array and find tru values and put them into new arry

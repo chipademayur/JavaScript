24a79771b5ee53e0d2690b971ba7c3a2c958dc68
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-23 22:41:49 +0530

    Add files via upload

---
+++ reduce.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4, 5];
+let ar = arr.reduce((h1, h2) => {
+  return h1 + h2;
+});
+console.log(ar);
+
+//the reduce method execute a reducer function for array element
+// the reduce() method returns a single value the function accumulated result.

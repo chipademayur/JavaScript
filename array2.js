761825a28a89ddbe2fc9ed6a1b5abfa1560e408a
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-23 15:42:42 +0530

    Add files via upload

---
+++ array2.js
@@ -0,0 +1,25 @@
+// Delete method in javascript
+// let num = [1, 2, 3, 4, 5];
+// let num2 = [11, 22, 31, 14, 15, 16, 16, 7];
+// delete num[0];
+// console.log(num);
+
+// concat method in javascrip
+// let num3 = num.concat(num2);
+// console.log(num3);
+
+// sort method in js
+// let num2 = [11, 22, 31, 14, 15, 16, 16, 7];
+// num2.sort;
+// console.log(num2);
+
+// splice and slice   slice can be used toa add new item to an array
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// num.splice(2 /*for index */, 3 /*delete item */, 11, 11, 11);
+// console.log(num);
+
+// slice in javascript
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let a = num.slice(1, 3);
+console.log(a);

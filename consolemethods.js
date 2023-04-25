43ec6ac712a1bf1041562b174264c427c1c9059d
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-25 18:14:13 +0530

    Add files via upload

---
+++ consolemethods.js
@@ -0,0 +1,13 @@
+console.assert(5 > 3); //write an assertion message if assertion fails
+
+console.clear(); //clear the console
+
+console.log(); //output a message to the console
+
+console.table(); //use for displaying in tabular form
+
+console.warn(); //output s warning message
+
+console.info(); //use for information message
+
+console.time(); //use to trsck how long an operation takes

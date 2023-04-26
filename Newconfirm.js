cee78b270283cd8f6d839da50feca89224a379f7
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-26 22:47:20 +0530

    Add files via upload

---
+++ Newconfirm.js
@@ -0,0 +1,3 @@
+confirm("press here");
+//confirm message sisplay a message box with a message an ok button and a cancel button
+// the confirm method return true if the user click ok otherwise false

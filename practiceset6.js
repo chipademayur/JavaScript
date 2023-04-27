6ad73cdf4aff2d826990ce67e94f4a36b789ec7d
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-27 12:48:50 +0530

    Add files via upload

---
+++ practiceset6.js
@@ -0,0 +1,14 @@
+// write a program using prompt function to take input of age as a value from the userand use alert to tell him if he deive or not
+
+// let age = prompt("Enter the age");
+// age=Number.parseInt(age)
+// if (age < 18 && age > 0) {
+//   alert("you cannot drive ");
+// } else {
+//   alert("you can drive");
+// }
+
+// while (age >= 18) {
+//   alert("you can drive");
+//   break;
+// }
